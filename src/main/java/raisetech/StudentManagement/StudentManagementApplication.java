a2264e46e878f9354e40ad1eaf7fa3e511b94c12
Ponta <simple.saron103@gmail.com>
2026-05-03 19:41:34 +0900

    第7回演習課題、CRUD処理の実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,10 +1,14 @@
 package raisetech.StudentManagement;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -27,5 +31,19 @@ public class StudentManagementApplication {
   @GetMapping("/studentCourses")
 	public List<StudentCourse> getStudentCourses() {
 		return repository.searchCourses();
+	}
+	@PostMapping("/student")
+	public void registerStudent(String name, int age){
+		repository.registerStudent(name,age);
+	}
+
+	@PatchMapping("/student")
+	public void updateStudent(String name, int age){
+		repository.updateStudent(name,age);
+	}
+
+	@DeleteMapping("/student")
+	public void deleteStudent(String name){
+		repository.deleteStudent(name);
 	}
 }
