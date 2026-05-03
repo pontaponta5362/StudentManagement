dbbef6856f85f367be55fb74128b8fdd508202bc
Pontao <simple.saron103@gmail.com>
2026-05-04 01:52:37 +0900

    Revert "[第7回課題] studentsテーブルのCRUD処理の実装"

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,49 +1,22 @@
 package raisetech.StudentManagement;
 
-import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	@Autowired
-	private StudentRepository repository;
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
 
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-  @GetMapping("/studentCourses")
-	public List<StudentCourse> getStudentCourses() {
-		return repository.searchCourses();
-	}
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name,age);
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
 	}
 
-	@PatchMapping("/student")
-	public void updateStudent(String name, int age){
-		repository.updateStudent(name,age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
 }
